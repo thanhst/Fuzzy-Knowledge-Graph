7087a89f29d6f837089cf7adfd4ea5fe01d7fd48
thanhst <thanhxk2003@gmail.com>
2025-03-26 17:21:56 +0700

    update

--- References/DDS_GUI/DDS_GUI/Extract.m
+++
@@ -1,8 +0,0 @@
-function [ features ] = Extract( select )
-%EXTRACT Summary of this function goes here
-%   Detailed explanation goes here
-    save select.mat select;
-    FeatureExtraction;
-    load resultFeature.mat;
-    features=resultFeature;
-end
