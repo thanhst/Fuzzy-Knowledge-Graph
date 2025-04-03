acc27d3d9b37e7f42eb0499fd6e304f1361bb127
thanhst <thanhxk2003@gmail.com>
2025-04-03 15:35:00 +0700

    Update: Rename folder Python to Source_code

--- Source_code/Main.py
+++ Source_code/Main.py
@@ -14,7 +14,7 @@ import pandas as pd
     # cluster = [2,2,3,3,3,2,2,2,2,2,2,3,3,3,3,2,3,2,3,2,2,2,5,5,5,5,5,5,5,5,5,5,6]
 print("Only Image Feature")
 FIS(fileName="Only Image Feature",
-    filePath="D:\Study\InternAIRC\source_code_Tan\source_code_Tan\Python\data\Dataset\OnlyImageFeature.csv",
+    filePath=".\data\Dataset\OnlyImageFeature.csv",
     cluster=[5,5,5,5,5,5,5,5,5,5,6])
 
 traindf = pd.read_csv('./data/FIS/output/Only Image Feature/Rule_List.csv')
@@ -34,7 +34,7 @@ fkg_instance.FKG(df = base,testdf=test,Turn=None,Modality="Only Image Feature")
 print("Only Table Feature Remove Missing")
 
 FIS(fileName="Only Table Feature Remove Missing",
-    filePath="D:\Study\InternAIRC\source_code_Tan\source_code_Tan\Python\data\Dataset\OnlyTableFeatureRemoveMissing.csv",
+    filePath=".\data\Dataset\OnlyTableFeatureRemoveMissing.csv",
     cluster=[3,5,2,3,2,3,2,2,2,6])
 traindf = pd.read_csv('./data/FIS/output/Only Table Feature Remove Missing/Rule_List.csv')
 testdf = pd.read_csv('./data/FIS/output/Only Table Feature Remove Missing/FRB/TestDataRule.csv')
@@ -61,7 +61,7 @@ print("Fusion Feature Remove Missing")
 
 
 FIS(fileName="Fusion Feature Remove Missing",
-    filePath="D:\Study\InternAIRC\source_code_Tan\source_code_Tan\Python\data\Dataset\FusionFeatureRemoveMissing.csv",
+    filePath=".\data\Dataset\FusionFeatureRemoveMissing.csv",
     cluster=[3,5,2,3,2,3,2,2,2,5,5,5,5,5,5,5,5,5,5,6])
 traindf = pd.read_csv('./data/FIS/output/Fusion Feature Remove Missing/Rule_List.csv')
 testdf = pd.read_csv('./data/FIS/output/Fusion Feature Remove Missing/FRB/TestDataRule.csv')
